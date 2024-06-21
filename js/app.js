f010777c8a9a455eac8811874e6580271e917351
Dize dev <axiozweb2022@gmail.com>
2024-06-21 00:46:50 -0500

    se creo el layout con javascript usando la libreria de muuri

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+const grid = new Muuri('.grid', {
+    layout: {
+        rounding: false
+    }
+});
+
+window.addEventListener('load', () => {
+    grid.refreshItems().layout();
+    document.getElementById('grid').classList.add('imagenes-cargadas');
+});
