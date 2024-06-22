40ad9d66ace1dafafc97f8060a894d7f15cf96ad
Dize dev <axiozweb2022@gmail.com>
2024-06-21 23:00:54 -0500

    se agrego la funcionalidad de filtrado por categorias

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
-
-const grid = new Muuri('.grid', {
-    layout: {
-        rounding: false
-    }
+const grid = new Muuri(".grid", {
+  layout: {
+    rounding: false,
+  },
 });
 
-window.addEventListener('load', () => {
-    grid.refreshItems().layout();
-    document.getElementById('grid').classList.add('imagenes-cargadas');
+window.addEventListener("load", () => {
+  grid.refreshItems().layout();
+  document.getElementById("grid").classList.add("imagenes-cargadas");
+
+  const enlaces = document.querySelectorAll("#categorias a");
+  enlaces.forEach((elemento) => {
+    elemento.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      enlaces.forEach((enlaces) => enlaces.classList.remove("activo"));
+      event.target.classList.add("activo");
+
+      const categorias = event.target.textContent.toLowerCase();
+
+      categorias === "todos"
+        ? grid.filter("[data-categoria]")
+        : grid.filter(`[data-categoria="${categorias}"]`);
+    });
+  });
 });
